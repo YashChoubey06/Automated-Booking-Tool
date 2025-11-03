1507206012ee79a92d09654672391ad4ef661609
Yash Choubey <yc.nitin.01@gmail.com>
2025-11-04 00:25:56 +0530

    Midnight Commit - 01

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ACCOUNT_EMAIL = "test@test.com"
+ACCOUNT_PASSWORD = "password"
+GYM_URL = "https://appbrewery.github.io/gym/"
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+driver = webdriver.Chrome(options=chrome_options)
 
+wait = WebDriverWait(driver, 2)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+driver.get(GYM_URL)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+email = driver.find_element(By.ID, "email-input")
+email.send_keys(ACCOUNT_EMAIL)
+password = driver.find_element(By.ID, "password-input")
+password.send_keys(ACCOUNT_PASSWORD)
+
+submit_button = driver.find_element(By.ID, "submit-button")
+submit_button.click()
+
+wait.until(ec.presence_of_element_located((By.ID, "schedule-page")))
+
